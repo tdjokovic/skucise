26ec8b9397946ed3210834af19ef596d782b99ec
Tijana Đoković <tdjokovic00@gmail.com>
2022-08-13 14:34:28 +0200

    Sitne izmene, popravljeni bagovi
    .

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { PropertiesComponent } from './properties/properties.component';
 
+// data - Niz uloga koje imaju pristup ruti/stranici, prazan niz daje dozvolu svim ulogama  
 const routes: Routes = [
-  {path:'', component:HomeComponent},
-  {path:'home', component:HomeComponent},
-  {path:'contactus', component:ContactComponent},
-  {path:'properties', component:PropertiesComponent},
-  {path:'about', component:AboutusComponent},
+  {path:'', component:HomeComponent,                 data: { allowedRoles: [] }},
+  {path:'home', component:HomeComponent,             data: { allowedRoles: [] }},
+  {path:'contactus', component:ContactComponent,     data: { allowedRoles: [] }},
+  {path:'properties', component:PropertiesComponent, data: { allowedRoles: [] }},
+  {path:'about', component:AboutusComponent,         data: { allowedRoles: [] }},
   
 ];
 
